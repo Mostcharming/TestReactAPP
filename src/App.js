3b2bd4b02eabf8f360e0631f2c728beb00a1910c
Mayowa F Fredrick <mostcharming920@yahoo.com>
2023-01-14 09:51:55 +0100

    simple react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+
+const url =
+  'http://zimauserservice-env.eu-west-2.elasticbeanstalk.com/api/v1/userapi/auth/register';
+var body = {
+  firstName: 'Ayobami',
+  lastName: 'Fadeni',
+  email: 'seyilegend1709@gmail.com',
+  platform: 'BRAND_SELLER',
+  gender: 'MALE',
+  password: 'String1@',
+  phoneNumber: 'String1@',
+  countryCode: 'NG',
+};
 
 function App() {
+  React.useEffect(() => {
+    axios.post(url, body).then((response) => console.log(response));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
